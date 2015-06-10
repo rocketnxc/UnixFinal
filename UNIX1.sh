0098e1105a22d5dc55d145686491090707cf5895
rocketnxc <rocketnxc@gmail.com>
2015-06-10 18:08:30 +0700

    Update UNIX1.sh

--- UNIX1.sh
+++ UNIX1.sh
@@ -1,9 +1,11 @@
+#!/bin/sh
+
 #!/bin/bash
 
 echo "Enter the path you want to search: "
 read PATH_NAME
 
-find $PATH_NAME -type f -regex ".*\.\(jpg\|gif\|png\|jpeg\)" -exec identify -format ' %f %d %e %b %G \n' {} \; 
+find $PATH_NAME -type f -regex ".*\.\(jpg\|gif\|png\|jpeg\)" -exec identify -format ' %f %d %e %b %G \n' {} >> test.html \; 
 
 echo "Choose how you want to sort the result: "
 echo "1. By Name"
@@ -15,19 +17,22 @@ echo "Please enter your choice: "
 read i
 
 case "$i" in
-	1) find $PATH_NAME -type f -regex ".*\.\(jpg\|gif\|png\|jpeg\)" -exec identify -format ' %f %d %e %b %G \n' {} \; | sort -k 1
+	1) find $PATH_NAME -type f -regex ".*\.\(jpg\|gif\|png\|jpeg\)" -exec identify -format ' %f %d %e %b %G \n' {} \; | sort -k 1 >> test.html
 	;;
-	2) find $PATH_NAME -type f -regex ".*\.\(jpg\|gif\|png\|jpeg\)" -exec identify -format ' %f %d %e %b %G \n' {} \; | sort -k 2
+	2) find $PATH_NAME -type f -regex ".*\.\(jpg\|gif\|png\|jpeg\)" -exec identify -format ' %f %d %e %b %G \n' {} \; | sort -k 2 >> test.html 
 	;;
-	3) find $PATH_NAME -type f -regex ".*\.\(jpg\|gif\|png\|jpeg\)" -exec identify -format ' %f %d %e %b %G \n' {} \; | sort -k 3
+	3) find $PATH_NAME -type f -regex ".*\.\(jpg\|gif\|png\|jpeg\)" -exec identify -format ' %f %d %e %b %G \n' {} \; | sort -k 3 >> test.html 
 	;;
-	4) find $PATH_NAME -type f -regex ".*\.\(jpg\|gif\|png\|jpeg\)" -exec identify -format ' %f %d %e %b %G \n' {} \; | sort -k 4
+	4) find $PATH_NAME -type f -regex ".*\.\(jpg\|gif\|png\|jpeg\)" -exec identify -format ' %f %d %e %b %G \n' {} \; | sort -k 4 >> test.html 
 	;;
-	5) find $PATH_NAME -type f -regex ".*\.\(jpg\|gif\|png\|jpeg\)" -exec identify -format ' %f %d %e %b %G \n' {} \; | sort -k 5
+	5) find $PATH_NAME -type f -regex ".*\.\(jpg\|gif\|png\|jpeg\)" -exec identify -format ' %f %d %e %b %G \n' {} \; | sort -k 5 >> test.html 
 	;;
 	*) echo "Wrong choice!"
 	;;
 esac
 
+find $PATH_NAME -type f -exec md5sum '{}' ';' | sort | uniq
+
 exit 0
+
 
